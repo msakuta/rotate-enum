860f3b1ce929be005c7e11851e949223d0703ec8
msakuta <masahiro.sakuta@gmail.com>
2021-01-24 10:02:47 +0900

    Add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,56 @@
+/// # rotate-enum crate
+/// 
+/// A simple macro that implements `prev()` and `next()` methods to an enum in Rust
+/// 
+/// ## Motivation
+/// 
+/// Sometimes you define an enum like this
+/// 
+/// ```Rust
+/// enum Direction {
+///     Up,
+///     Left,
+///     Down,
+///     Right,
+/// }
+/// ```
+/// 
+/// and you want to rotate them in some logic,
+/// 
+/// ```Rust
+/// let up = Direction::Up;
+/// let left = Direction::Left;
+/// let down = Direction::Down;
+/// let right = Direction::Right;
+/// 
+/// assert!(up.next() == left);
+/// assert!(left.next() == down);
+/// assert!(down.next() == right);
+/// assert!(right.next() == up);
+/// ```
+/// 
+/// You can of course implement these methods manually, but it's repetitive and error prone.
+/// Don't you think it should be automated?
+/// This crate provides a `RotateEnum` derive macro to just do this.
+/// 
+/// 
+/// ## Usage
+/// 
+/// ```rust
+/// use rotate_enum::RotateEnum;
+/// 
+/// #[derive(RotateEnum)]
+/// enum Direction {
+///     Up,
+///     Left,
+///     Down,
+///     Right,
+/// }
+/// ```
+/// 
+
+
+
 use core::panic;
 
 use proc_macro::TokenStream;
