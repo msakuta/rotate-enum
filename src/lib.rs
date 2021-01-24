57aef04033a5dbb19bf77d997ae5d449c5caafc7
msakuta <masahiro.sakuta@gmail.com>
2021-01-24 10:17:46 +0900

    Move docstring to crate root with inner documentation comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,66 @@
-/// # rotate-enum crate
-/// 
-/// A simple macro that implements `prev()` and `next()` methods to an enum in Rust
-/// 
-/// ## Motivation
-/// 
-/// Sometimes you define an enum like this
-/// 
-/// ```
-/// enum Direction {
-///     Up,
-///     Left,
-///     Down,
-///     Right,
-/// }
-/// ```
-/// 
-/// and you want to rotate them in some logic,
-/// 
-/// ```
-/// # use rotate_enum::RotateEnum;
-/// # #[derive(RotateEnum, PartialEq, Clone, Copy)]
-/// # enum Direction {
-/// #     Up,
-/// #     Left,
-/// #     Down,
-/// #     Right,
-/// # }
-/// let up = Direction::Up;
-/// let left = Direction::Left;
-/// let down = Direction::Down;
-/// let right = Direction::Right;
-/// 
-/// assert!(up.next() == left);
-/// assert!(left.next() == down);
-/// assert!(down.next() == right);
-/// assert!(right.next() == up);
-/// 
-/// assert!(up.prev() == right);
-/// assert!(left.prev() == up);
-/// assert!(down.prev() == left);
-/// assert!(right.prev() == down);
-/// ```
-/// 
-/// You can of course implement these methods manually, but it's repetitive and error prone.
-/// Don't you think it should be automated?
-/// This crate provides a `RotateEnum` derive macro to just do this.
-/// 
-/// 
-/// ## Usage
-/// 
-/// ```rust
-/// use rotate_enum::RotateEnum;
-/// 
-/// #[derive(RotateEnum)]
-/// enum Direction {
-///     Up,
-///     Left,
-///     Down,
-///     Right,
-/// }
-/// ```
-/// 
+//! # rotate-enum crate
+//! 
+//! This crate provides a simple macro that implements `prev()` and `next()` methods to an enum.
+//! 
+//! ## Motivation
+//! 
+//! Sometimes you define an enum like this
+//! 
+//! ```
+//! enum Direction {
+//!     Up,
+//!     Left,
+//!     Down,
+//!     Right,
+//! }
+//! ```
+//! 
+//! and you want to rotate them in some logic,
+//! 
+//! ```
+//! # use rotate_enum::RotateEnum;
+//! # #[derive(RotateEnum, PartialEq, Clone, Copy)]
+//! # enum Direction {
+//! #     Up,
+//! #     Left,
+//! #     Down,
+//! #     Right,
+//! # }
+//! let up = Direction::Up;
+//! let left = Direction::Left;
+//! let down = Direction::Down;
+//! let right = Direction::Right;
+//! 
+//! assert!(up.next() == left);
+//! assert!(left.next() == down);
+//! assert!(down.next() == right);
+//! assert!(right.next() == up);
+//! 
+//! assert!(up.prev() == right);
+//! assert!(left.prev() == up);
+//! assert!(down.prev() == left);
+//! assert!(right.prev() == down);
+//! ```
+//! 
+//! You can of course implement these methods manually, but it's repetitive and error prone.
+//! Don't you think it should be automated?
+//! This crate provides a `RotateEnum` derive macro to just do this.
+//! 
+//! 
+//! ## Usage
+//! 
+//! ```rust
+//! use rotate_enum::RotateEnum;
+//! 
+//! #[derive(RotateEnum)]
+//! enum Direction {
+//!     Up,
+//!     Left,
+//!     Down,
+//!     Right,
+//! }
+//! ```
+//! 
 
 
 
